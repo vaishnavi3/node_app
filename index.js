1d0e26c02563c67f9a771a823f215997a8b7d6ff
Vaishnavi <bharadevaishnavi@gmail.com>
2019-12-04 11:37:33 +0530

    WIP-add authentication to users

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var http = require('http');
 var url = require('url');
 var stringDecoder = require('string_decoder').StringDecoder;
-var config = require('./config');
+var config = require('./lib/config');
+var handlers = require('./lib/handlers');
+var helpers = require('./lib/helpers');
 
 var server = http.createServer(function(req, res){
   var parsedUrl = url.parse(req.url, true);
@@ -30,7 +32,7 @@ var server = http.createServer(function(req, res){
       'queryStringObject': queryStringObject,
       'method': method,
       'headers': headers,
-      'payload': buffer
+      'payload': helpers.parseJsonToBbject(buffer)
 
     };
 
@@ -53,16 +55,9 @@ server.listen(config.port, function(){
   console.log("The server is listening on port: "+ config.port+ " in "+config.envName+" mode" );
 })
 
-var handlers = {};
-
-handlers.notFound = function(data, callback){
-  callback(404);
-};
-
-handlers.ping = function(data, callback){
-  callback(200);
-};
 
 var router = {
-  'ping': handlers.ping
+  'ping': handlers.ping,
+  'users': handlers.users,
+  'tokens': handlers.tokens
 }
